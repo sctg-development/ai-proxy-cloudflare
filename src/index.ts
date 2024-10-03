f340f698bf8c2673d5be79c07c1ba803ac17cdd0
Ronan <ronan.le_meillat@highcanfly.club>
2024-10-03 13:05:53 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,68 +4,90 @@
 
 async function handleRequest(request: Request, env: Env): Promise<Response> {
   const { pathname } = new URL(request.url);
-  const [_, token, next, ...params] = pathname.split('/');
+  const [_, token, next, ...params] = pathname.split("/");
 
   if (/^v\d+$/.test(token)) {
     return proxy(request, env);
   } else if (token === env.ACCESS_TOKEN) {
-    console.log('Accessing master handler');
+    console.log("Accessing master handler");
     let result: string | undefined;
-    if (request.method === 'DELETE') {
+    if (request.method === "DELETE") {
       await deleteUser(next, env);
-      result = 'ok';
-    } else if (next === 'register' || next === 'reset') {
+      result = "ok";
+    } else if (next === "addkey") {
+      console.log(`Adding key ${params[1]} for host ${params[0]}`);
+      await addkey(params[0], params[1], env);
+      result = "ok";
+    } else if (next === "register" || next === "reset") {
       result = await registerUser(params[0], env);
     }
 
-    if (!result) throw 'Invalid action';
+    if (!result) throw "Invalid action";
     return new Response(`${result}\n`, {
-      headers: { 'Content-Type': 'text/plain' }
+      headers: { "Content-Type": "text/plain" },
     });
   }
-  throw 'Access forbidden';
+  throw "Access forbidden";
 }
 
 async function proxy(request: Request, env: Env): Promise<Response> {
   const headers = new Headers(request.headers);
-  const authKey = 'Authorization';
-  const token = headers.get(authKey)?.split(' ').pop();
-  if (!token) throw 'Auth required';
+  const authKey = "Authorization";
+  const token = headers.get(authKey)?.split(" ").pop();
+  if (!token) throw "Auth required";
 
   // validate user
-  const users: Record<string, { key: string }> = await env.KV_AI_PROXY.get("users", { type: 'json' }) || {};
+  const users: Record<string, { key: string }> =
+    (await env.KV_AI_PROXY.get("users", { type: "json" })) || {};
   let name: string | undefined;
-  for (let key in users)
-    if (users[key].key === token)
-      name = key;
+  for (let key in users) if (users[key].key === token) name = key;
 
-  if (!name) throw 'Invalid token';
+  if (!name) throw "Invalid token";
   console.log(`User ${name} acepted.`);
 
   // proxy the request
   const url = new URL(request.url);
   // 1. Check if the request include the final host in the X-Host-Final header
-  const host = headers.get('X-Host-Final');
+  const host = headers.get("X-Host-Final");
   // 2. replace url.host whit the host from the X-Host-Final header or 'api.openai.com' if not present
-  url.host = host || 'api.openai.com';
+  url.host = host || "api.openai.com";
   // 3. Check if an API ley is present in the KV for this host
-  const apiKey = await env.KV_AI_PROXY.get(url.host, { type: 'text' });
+  const apiKey = await env.KV_AI_PROXY.get(host || env.OPENAPI_API_KEY);
+
+  console.log(`API key for host ${host} is ${apiKey}`);
   // 4. replace with the real API key
-  headers.set(authKey, `Bearer ${apiKey || env.OPENAPI_API_KEY}`);
+  headers.set(authKey, `Bearer ${apiKey}`);
+
+  // Force to use https on port 443
+  if (url.protocol !== "https:") {
+    url.protocol = "https:";
+  }
+
+  if (url.port !== "443") {
+    url.protocol = "https:";
+    url.port = "";
+  }
+
+  console.log(`Proxying request to ${url.toString()} with key ${apiKey}`);
   // 5. issue the underlying request
   // Only pass body if request method is not 'GET'
-  const requestBody = request.method !== 'GET' ? JSON.stringify(await request.json()) : null;
+  const requestBody =
+    request.method !== "GET" ? JSON.stringify(await request.json()) : null;
   return fetch(url.toString(), {
     method: request.method,
-    headers: {...headers},
-    body: requestBody
+    headers: { ...headers },
+    body: requestBody,
   });
 }
 
-async function registerUser(user: string | undefined, env: Env): Promise<string> {
-  if (!user?.length) throw 'Invalid username1';
+async function registerUser(
+  user: string | undefined,
+  env: Env
+): Promise<string> {
+  if (!user?.length) throw "Invalid username1";
 
-  const users: Record<string, { key: string }> = await env.KV_AI_PROXY.get("users", { type: 'json' }) || {};
+  const users: Record<string, { key: string }> =
+    (await env.KV_AI_PROXY.get("users", { type: "json" })) || {};
   const key = generateAPIKey();
   users[user] = { key };
   await env.KV_AI_PROXY.put("users", JSON.stringify(users));
@@ -73,18 +95,25 @@ async function registerUser(user: string | undefined, env: Env): Promise<string>
 }
 
 async function deleteUser(user: string | undefined, env: Env): Promise<void> {
-  if (!user?.length) throw 'Invalid username2';
+  if (!user?.length) throw "Invalid username2";
 
-  const users: Record<string, { key: string }> = await env.KV_AI_PROXY.get("users", { type: 'json' }) || {};
-  if (!users[user]) throw 'User not found';
+  const users: Record<string, { key: string }> =
+    (await env.KV_AI_PROXY.get("users", { type: "json" })) || {};
+  if (!users[user]) throw "User not found";
 
   delete users[user];
   await env.KV_AI_PROXY.put("users", JSON.stringify(users));
 }
 
+async function addkey(host: string, key: string, env: Env): Promise<void> {
+  if (!host || !key) throw "Invalid host or key";
+  await env.KV_AI_PROXY.put(host, key);
+}
+
 function generateAPIKey(): string {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let apiKey = 'sk-cfw';
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let apiKey = "sk-cfw";
 
   for (let i = 0; i < 45; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
@@ -96,6 +125,8 @@ function generateAPIKey(): string {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    return handleRequest(request, env).catch(err => new Response(err || 'Unknown reason', { status: 403 }))
-  }
+    return handleRequest(request, env).catch(
+      (err) => new Response(err || "Unknown reason", { status: 403 })
+    );
+  },
 };
---
+++ tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOCAL_ADMIN_KEY="random_production_value"
+NEW_HOST=${NEW_HOST:-"api.openai.com"}
+NEW_HOST_KEY=${NEW_HOST_KEY:-"osk-fdwvR2IAwWX6hpygKNH3DvKuJZ92hBEuanZCGyxVI7X9zRez"}
+# TEST: Create a user
+# Create a random user name with 12 characters
+USERNAME=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+USERKEY=$(curl http://localhost:8787/$LOCAL_ADMIN_KEY/register/$USERNAME)
+echo "Username: $USERNAME Key: $USERKEY"
+
+# TEST: add an host key
+curl http://localhost:8787/$LOCAL_ADMIN_KEY/addkey/$NEW_HOST/$NEW_HOST_KEY
+
+# TEST: Try to retrieve models
+curl -H "Authorization: Bearer $USERKEY" -H "X-Host-Final: $NEW_HOST" http://localhost:8787/openai/v1/models
+
+# Delete the user
+curl -X DELETE http://localhost:8787/$LOCAL_ADMIN_KEY/$USERNAME
+
